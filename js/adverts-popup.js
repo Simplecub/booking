2c6b04bc46c043b9d4cf929eed0d804bbf227281
Simplecub <exdimail@gmail.com>
2022-07-14 13:58:35 +0400

    adverts_popup

--- js/adverts-popup.js
+++ js/adverts-popup.js
@@ -5,13 +5,18 @@ const mapCanvasElement = document.querySelector('#map-canvas')
 
 const getPopup = (item) => {
   let card = cardElement.cloneNode(true)
-  card.querySelector('.popup__title').textContent = item.offer.title
+  const modifiers = item.offer.features.map((feat) => 'popup__feature--' + feat)
+  card.querySelectorAll('.popup__feature').forEach((listItem) => {
+    const modifier = listItem.classList[1]
+    if (!modifiers.includes(modifier)) {listItem.remove()}
+  })
+    card.querySelector('.popup__title').textContent = item.offer.title
   card.querySelector('.popup__text--address').textContent = item.offer.address
   card.querySelector('.popup__text--price').innerHTML = `${item.offer.price} <span>₽/ночь</span>`
-  card.querySelector('.popup__type').textContent = item.offer.type
+ // card.querySelector('.popup__type').textContent = item.offer.type
   card.querySelector('.popup__text--capacity').textContent = `${item.offer.rooms} комнаты для ${item.offer.guests} гостей`
   card.querySelector('.popup__text--time').textContent = `Заезд после ${item.offer.checkin}, выезд до ${item.offer.checkout}`
-  card.querySelector('.popup__features').textContent = item.offer.features
+//  card.querySelector('.popup__features').textContent = item.offer.features
   card.querySelector('.popup__description').textContent = item.offer.description
  // card.querySelector('.popup__photos').querySelector('.popup__photo').src = item.offer.photos
   card.querySelector('.popup__avatar').src = item.author.avatar
