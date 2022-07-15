46d0ad5c21a60fc288619f736d050a4c8b8fadbb
Simplecub <exdimail@gmail.com>
2022-07-15 12:12:22 +0400

    adverts_popup+validate

--- js/validate.js
+++ js/validate.js
@@ -1,20 +1,27 @@
 const adFormElement = document.querySelector('.ad-form')
 const submitButton = adFormElement.querySelector('.ad-form__submit')
 
+
+adFormElement.querySelectorAll('input').forEach((item) => {
+  if (item.hasAttribute('required')) {
+    item.setAttribute('data-pristine-required-message', "Обязательно для заполнения")
+  }
+})
+
 const config = {
-  classTo: 'ad-form__element',
-  errorClass: 'ad-form--invalid',
-  successClass: 'ad-form--valid',
-  errorTextParent: 'ad-form__element',
-  errorTextTag: 'span',
-  errorTextClass: 'form__error'
+  classTo: 'ad-form__element', //выбор элементов для валидации
+  errorClass: 'form__error',  //добавляет класс (для изменения цвета текста если валидация не пройдена)
+  successClass: 'ad-form--valid', //добавляет класс ad-form--valid (не используется)
+  errorTextParent: 'ad-form__element', //родительский элемент для которого будет отображена ошибка валидации
+  errorTextTag: 'span',   //сообщение ошибки выводится - как строка
+  errorTextClass: 'form__error' //к сообщению об ошибки применяется класс ..'form__error'
 };
 const addValidator = () => {
   const pristine = new Pristine(adFormElement, config);
   pristine.validate();
 
   adFormElement.onsubmit = (evt) => {
-    if (!pristine.validate() || 0 ) {
+    if (!pristine.validate() || 0) {
       console.log('errr');
       evt.preventDefault()
     }
