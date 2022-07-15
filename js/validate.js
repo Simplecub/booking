5960d23837eb72538a19f9ec97349751f3ff1de1
Simplecub <exdimail@gmail.com>
2022-07-15 17:47:25 +0400

    adverts_popup+validate+

--- js/validate.js
+++ js/validate.js
@@ -1,5 +1,17 @@
 const adFormElement = document.querySelector('.ad-form')
 const submitButton = adFormElement.querySelector('.ad-form__submit')
+const rooms = adFormElement.querySelector('#room_number')
+const capacity = adFormElement.querySelector('#capacity')
+const elem = adFormElement.querySelector('.ad-form__header')
+
+const CONFIG_PRISTINE = {
+  classTo: 'ad-form__element', //выбор элементов для валидации
+  errorClass: 'form__error',  //добавляет класс (для изменения цвета текста если валидация не пройдена)
+  successClass: 'ad-form--valid', //добавляет класс ad-form--valid (не используется)
+  errorTextParent: 'ad-form__element', //родительский элемент для которого будет отображена ошибка валидации
+  errorTextTag: 'span',   //сообщение ошибки выводится - как строка
+  errorTextClass: 'form__error' //к сообщению об ошибки применяется класс ..'form__error'
+};
 
 
 adFormElement.querySelectorAll('input').forEach((item) => {
@@ -8,26 +20,44 @@ adFormElement.querySelectorAll('input').forEach((item) => {
   }
 })
 
-const config = {
-  classTo: 'ad-form__element', //выбор элементов для валидации
-  errorClass: 'form__error',  //добавляет класс (для изменения цвета текста если валидация не пройдена)
-  successClass: 'ad-form--valid', //добавляет класс ad-form--valid (не используется)
-  errorTextParent: 'ad-form__element', //родительский элемент для которого будет отображена ошибка валидации
-  errorTextTag: 'span',   //сообщение ошибки выводится - как строка
-  errorTextClass: 'form__error' //к сообщению об ошибки применяется класс ..'form__error'
-};
+let msg = []
+const getRoomsCapacity = () => {
+  msg.length = 0
+  if (rooms.value === '100' && capacity.value !== '0') {
+    msg.push('Для 100 комнат выберите - не для гостей')
+    console.log('100*!0')
+    return !msg.length
+  }
+  if (rooms.value !== '100' && capacity.value === '0') {
+    msg.push('Для не для гостей выберите - 100 комнат')
+    console.log('!100*0')
+    return !msg.length
+  }
+  if (rooms.value < capacity.value) {
+    msg.push('Комнат не может быть меньше гостей')
+    console.log('Комнат не может быть меньше гостей');
+    return !msg.length
+  }
+  if (rooms.value >= capacity.value) {
+    console.log('good');
+    return true
+  }
+  return !msg.length
+}
+
+Pristine.addValidator('rooms', getRoomsCapacity, () => msg, 5, false)
+Pristine.addValidator('capacity', getRoomsCapacity, () => msg, 5, false)
+
 const addValidator = () => {
-  const pristine = new Pristine(adFormElement, config);
+  const pristine = new Pristine(adFormElement, CONFIG_PRISTINE);
   pristine.validate();
 
   adFormElement.onsubmit = (evt) => {
     if (!pristine.validate() || 0) {
-      console.log('errr');
+      console.log('stop-send-error');
       evt.preventDefault()
     }
   }
-
-
 }
 
 
