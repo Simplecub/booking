6c36a190897c1be567cbffcbbba419a5deafced2
Simplecub <exdimail@gmail.com>
2022-07-19 17:23:10 +0400

    add filter-map

--- js/filter-map.js
+++ js/filter-map.js
@@ -4,14 +4,25 @@ const filterFormElement = document.querySelector('.map__filters')
 const disableFilter = () =>{
   filterFormElement.classList.add('map__filters--disabled')
   filterFormElement.querySelectorAll('select').forEach((item) => item.setAttribute('disabled', 'disabled'))
-
 }
 
 const enableFilter = () => {
   filterFormElement.classList.remove('map__filters--disabled')
   filterFormElement.querySelectorAll('select').forEach((item) => item.disabled = false)
+}
+
+class Filter {
+
+
+
+
 
 }
 
 
+
+const getFiltered = () => {
+  filterFormElement.addEventListener('change', (evt) => {console.log(evt.target.value)})
+}
+getFiltered()
 export {enableFilter, disableFilter}
